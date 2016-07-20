25e82a88846c6b6719a62e5291ddac364c462933
Alex Seidler <alex.seidler@futurice.com>
2016-07-20 13:13:32 +0300

    Separate logic from TaskCard to TaskList

--- src/chores/task-card.jsx
+++ src/chores/task-card.jsx
@@ -1,26 +1,19 @@
 import React, {PropTypes} from 'react';
-import {connect} from 'refirebase';
 
-// TODO: Are you sure dialog when removing the task
-// TODO: Move removeTask logic (and groupId) back to TaskList
-// TODO: Also make dem damn models and hold id's in the models
-export const TaskCardView = ({taskId, task, isAdmin, removeTask}) =>
+export const TaskCard = ({task, isAdmin, removeTask, createDeed}) =>
   <article className="task-card">
     <h1>{task.name}</h1>
     <h2>{task.value}</h2>
     <p>{task.description}</p>
+    <button onClick={createDeed}>I did this!</button>
     {isAdmin && <button onClick={removeTask}>Remove</button>}
   </article>;
 
-TaskCardView.propTypes = {
-  taskId: PropTypes.string.isRequired,
+TaskCard.propTypes = {
   task: PropTypes.object.isRequired,
   isAdmin: PropTypes.bool,
-  removeTask: PropTypes.func.isRequired
+  removeTask: PropTypes.func.isRequired,
+  createDeed: PropTypes.func.isRequired
 };
 
-const TaskCard = connect(null, (firebase, {groupId, taskId}) => ({
-  removeTask: () =>
-    firebase.database().ref(`/groups/${groupId}/tasks/${taskId}`).remove()
-}))(TaskCardView);
 export default TaskCard;
--- src/chores/task-list.jsx
+++ src/chores/task-list.jsx
@@ -1,14 +1,16 @@
 import React, {PropTypes} from 'react';
 import {map} from 'lodash';
+import {compose, withHandlers} from 'recompose';
+import {v4} from 'uuid';
 import {connect} from 'refirebase';
-import {compose, adminProvider} from 'chores/view-utils';
+import {adminProvider} from 'chores/view-utils';
 import TaskCard from 'chores/task-card';
 
-export const TaskListView = ({groupId, isAdmin, tasks}) =>
+export const TaskListView = ({groupId, isAdmin, tasks, removeTaskFactory, createDeedFactory}) =>
   <ul className="task-list">
   {map(tasks, (task, id) =>
     <li className="task-list__item" key={id}>
-      <TaskCard isAdmin={isAdmin} taskId={id} task={task} />
+      <TaskCard isAdmin={isAdmin} task={task} removeTask={removeTaskFactory(id)} createDeed={createDeedFactory(id)} />
     </li>
   )}
   </ul>;
@@ -16,14 +18,28 @@ export const TaskListView = ({groupId, isAdmin, tasks}) =>
 TaskListView.propTypes = {
   groupId: PropTypes.string,
   isAdmin: PropTypes.boolean,
-  tasks: PropTypes.object
+  tasks: PropTypes.object,
+  removeTaskFactory: PropTypes.func.isRequired,
+  createDeedFactory: PropTypes.func.isRequired
 };
 
 const TaskList = compose(
+  adminProvider,
   connect(({groupId}) => ({
     tasks: groupId && `/groups/${groupId}/tasks`
+  }), (firebase, {groupId}) => ({
+    removeTask: taskId =>
+      firebase.database().ref(`/groups/${groupId}/tasks/${taskId}`).remove(),
+    createDeed: deed =>
+      firebase.database().ref(`/groups/${groupId}/deeds/${v4()}`).set(deed)
   })),
-  adminProvider
+  withHandlers({
+    removeTaskFactory: ({removeTask}) => taskId => () =>
+      removeTask(taskId),
+    // TODO: Make dem damn models and hold id's in the models (and utils!)
+    createDeedFactory: ({createDeed, user}) => taskId => () =>
+      createDeed({taskId, memberId: user.uid})
+  })
 )(TaskListView);
 
 export default TaskList;
